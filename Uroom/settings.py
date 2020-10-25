9725f5b7111c749d63f6cc113e80002854379757
dewy03 <64489898+dewy03@users.noreply.github.com>
2020-10-25 20:45:11 +0900

    Update static

--- Uroom/settings.py
+++ Uroom/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'myprofile','static')
+    os.path.join(BASE_DIR,'uroomapp','static')
 ]
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
